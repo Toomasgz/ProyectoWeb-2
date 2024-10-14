62a6e8d39f79cd91c78a908d4b82eb88152027b4
valencansina <cansinavalen@gmail.com>
2024-10-14 20:02:41 -0300

    se agregaron carpetas, archivo .htaccess y la base de datos .sql

--- club_embajadores.sql
+++ club_embajadores.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 18-09-2024 a las 14:16:53
+-- Tiempo de generación: 15-10-2024 a las 00:40:10
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.0.30
 
@@ -28,21 +28,12 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `cuotas` (
-  `id_cuota` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
   `deporte` varchar(50) NOT NULL,
   `precio` decimal(10,0) NOT NULL,
-  `categoria` varchar(50) NOT NULL,
+  `categoria` int(11) NOT NULL,
   `id_socio` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `cuotas`
---
-
-INSERT INTO `cuotas` (`id_cuota`, `deporte`, `precio`, `categoria`, `id_socio`) VALUES
-(1, 'basquet', 15301, 'u14', 1),
-(2, 'atletismo', 5250, 'u18', 2),
-(3, 'fulbo', 0, 'u20', 3);
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 -- --------------------------------------------------------
 
@@ -52,20 +43,21 @@ INSERT INTO `cuotas` (`id_cuota`, `deporte`, `precio`, `categoria`, `id_socio`) 
 
 CREATE TABLE `socios` (
   `id_socio` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `documento` varchar(20) NOT NULL,
-  `mail` varchar(100) NOT NULL,
-  `fecha_nacimiento` date NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+  `nombre` varchar(50) NOT NULL,
+  `apellido` varchar(50) NOT NULL,
+  `documento` int(11) NOT NULL,
+  `gmail` varchar(50) NOT NULL,
+  `fecha_de_nacimiento` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
 -- Volcado de datos para la tabla `socios`
 --
 
-INSERT INTO `socios` (`id_socio`, `nombre`, `documento`, `mail`, `fecha_nacimiento`) VALUES
-(1, 'pepito', '43040333', 'pepito123@gmail.com', '2024-09-02'),
-(2, 'juan ', '46013123', 'juancapo123@gmail.com', '2024-09-03'),
-(3, 'messi', '33016244', 'messigoat@gmail.com', '2024-09-11');
+INSERT INTO `socios` (`id_socio`, `nombre`, `apellido`, `documento`, `gmail`, `fecha_de_nacimiento`) VALUES
+(1, 'tomas ', 'gotierrez', 46115077, 'tomasgz@gmail.com', '2004-09-01'),
+(2, 'valentino', 'cansina', 47060175, 'valentino@gmail.com', '2006-02-20'),
+(3, 'lionel', 'messi', 32550148, 'messigoat@gmail.com', '1987-06-24');
 
 --
 -- Índices para tablas volcadas
@@ -75,15 +67,17 @@ INSERT INTO `socios` (`id_socio`, `nombre`, `documento`, `mail`, `fecha_nacimien
 -- Indices de la tabla `cuotas`
 --
 ALTER TABLE `cuotas`
-  ADD PRIMARY KEY (`id_cuota`),
-  ADD KEY `id_socio` (`id_socio`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `id_socio` (`id`),
+  ADD UNIQUE KEY `id_socio_2` (`id_socio`);
 
 --
 -- Indices de la tabla `socios`
 --
 ALTER TABLE `socios`
   ADD PRIMARY KEY (`id_socio`),
-  ADD UNIQUE KEY `documento` (`documento`);
+  ADD UNIQUE KEY `documento` (`documento`),
+  ADD UNIQUE KEY `gmail` (`gmail`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
@@ -93,7 +87,7 @@ ALTER TABLE `socios`
 -- AUTO_INCREMENT de la tabla `cuotas`
 --
 ALTER TABLE `cuotas`
-  MODIFY `id_cuota` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT de la tabla `socios`
